8bdc122a68e6f34d0f23ed1d9174302a5cae3231
Gerardo Pérez <ISnape.jilc45gmail.com>
2020-05-13 12:57:15 -0500

    Herencia

--- core/views.py
+++ core/views.py
@@ -1,36 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about-me/">Acerca de</a></li>
-    <li><a href="/portfolio/">Portafolio</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
-
 # Create your views here.
 def home(request):
-    return HttpResponse(html_base + """
-    <h2>Portada</h2>
-    <p>Esta es la portada</p>
-    """)
+    return render(request, "core/home.html")
     
 def about(request):
-    return HttpResponse(html_base + """
-    <h2>Acerca de</h2>
-    <p>Este es el parrafo</p>
-    """)
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(html_base + """
-    <h2>Portafolio</h2>
-    <p>Algunos de mis trabajos</p>
-    """)
+    return render(request, "core/portfolio.html")
     
 def contact(request):
-    return HttpResponse(html_base + """
-    <h2>Contacto</h2>
-    <p>Estos son mis datos de contacto</p>
-    """)
+    return render(request, "core/contact.html")
