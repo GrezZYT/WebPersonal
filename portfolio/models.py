74c2a17bf92871c2c239437090899c0745f7a319
Gerardo Pérez <ISnape.jilc45gmail.com>
2020-05-14 01:50:07 -0500

    Final

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,8 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name= "Titulo")
     description = models.TextField(verbose_name= "Descripcion")
     image = models.ImageField(verbose_name= "Imagen", upload_to="projects")
+    link = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name= "Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name= "Fecha de edicion")
+    
 
     class Meta:
         verbose_name = "proyecto"
